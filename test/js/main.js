f3e0976ff0830da7452678dd5aeeb32ec9d6fce6
Jason Lai <jase_lai@hotmail.com>
2016-10-19 13:27:03 +0100

    responsive navbar and subfooter 1 added

--- test/js/main.js
+++ test/js/main.js
@@ -1,10 +1,30 @@
 $(function (){
   console.log("js loaded");
 
+  $navItems = null;
+
   $("nav ul li:nth-child(1)")
   .on("click", function() {
-    console.log("clicked")
+
     event.preventDefault();
-    $(this).siblings().slideToggle();
+
+    $navItems = $(this).siblings();
+
+    $navItems.slideToggle();
   });
-})
+
+
+  $(window).on('resize', function (){
+
+    if ($(window).width() > 690 && $navItems !== null){
+      $navItems.show().css('display', 'inline');
+      $("nav ul li:nth-child(2)").hide();
+    }
+    else if($(window).width() < 690 && $navItems !== null && $navItems.is(':visible')) {
+        $navItems.hide();
+      }
+  });
+
+  
+});
+
